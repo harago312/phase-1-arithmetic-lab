31a28d2b50ecb240a4f2cc585d6e407b33a3a2f9
harago312 <ayisse312@gmail.com>
2022-11-28 22:39:46 +1000

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
 // Write your code here
+  let num1 = 31
+  let num2 = 2
+  var multiply= num1*num2
+
+  const random = Math.floor(Math.random()*10)
+
+ let num3 = 4
+ let num4 = 20
+ let mod = num3 % num4
+
+ let max = Math.max(2,3,5,7,20)
